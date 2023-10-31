275a8920c1a7febc44708bb44677464f5f48e614
codeandjoy <87646754+codeandjoy@users.noreply.github.com>
2023-10-31 12:47:54 +0200

    Basic setup. Created Search component. Added icons. Setup scss in config.

---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,53 @@
+<script>
+    import "@fontsource/poppins/700.css"
+
+    let searchData = "";
+
+    function querySearch(){
+        console.log(searchData);
+    }
+</script>
+
+
+
+<div class="city-search">
+    <input class="search-inp" type="text" placeholder="City:" bind:value={searchData}/>
+    <button class="search-btn" on:click={ querySearch }></button>
+</div>
+
+
+
+<style lang="scss">
+    .city-search{
+        display: flex;
+        align-items: center;
+
+        width: 1000px;
+
+        border-bottom: 5px solid #fff;
+        
+        .search-inp{
+            width: 100%;
+            
+            font-family: 'Poppins', sans-serif;
+            font-size: 100px;
+
+            text-align: center;
+
+            color: #fff;
+            background-color: transparent;
+            border: none;
+            outline: none;
+        }
+    
+        .search-btn{
+            width: 50px;
+            height: 50px;
+    
+            background-image: url('/icons/search.svg');
+    
+            background-color: transparent;
+            border: none;
+        }
+    }    
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Search from "../lib/components/Search.svelte";
+</script>
+
+
+
+<svelte:head>
+    <title>Test</title>
+</svelte:head>
+
+<div class="delta-weather-container">
+    <Search/>
+</div>
+
+
+
+<style>
+    :global(body){
+        margin: 0;
+        padding: 0;
+
+        background-color: #121212;
+
+    }
+    :global(*){
+        border-color: red;
+    }
+
+    .delta-weather-container{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        width: 100%;
+        height: 100vh;
+    }
+</style>
